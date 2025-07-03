a5d5209b97ea2ed76c5afbc0a812f979b23265a6
BuiBinhMinh <buiminh19102004@gmail.com>
2025-07-03 10:44:38 +0700

    upload

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     images: {
-    // Nếu dùng ảnh ngoài, giữ lại domain
     domains: ["scontent.fsgn15-1.fna.fbcdn.net"],
     },
 };
